b769aa2c5c9a7f93bc2668ef581a2b15b0834fa5
Mahmoud Darwish <imahmwd14@gmail.com>
2022-04-27 02:59:03 +0700

    added SearchAndResult.kt for better managing searches

--- domain/src/main/java/com/mahmoud_darwish/domain/repository/IBooksRepository.kt
+++ domain/src/main/java/com/mahmoud_darwish/domain/repository/IBooksRepository.kt
@@ -1,11 +1,13 @@
 package com.mahmoud_darwish.domain.repository
 
+import com.mahmoud_darwish.domain.model.SearchAndResult
 import com.mahmoud_darwish.domain.model.Volume
 import com.mahmoud_darwish.domain.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface IBooksRepository {
-    fun searchByTitle(title: String): Flow<Resource<List<Volume>>>
+    fun setSearchQuery(title: String)
+    val searchAndResult: Flow<Resource<SearchAndResult<List<Volume>>>>
     fun getVolumeById(id: String): Flow<Resource<Volume>>
 }
 
