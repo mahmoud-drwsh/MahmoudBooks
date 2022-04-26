2cd3e80f14cd162cb8234311494298c7a7fdd677
Mahmoud Darwish <imahmwd14@gmail.com>
2022-04-27 02:55:36 +0700

    migrated to kotlin flows

--- data/src/main/java/com/mahmoud_darwish/data/local/VolumeEntityDao.kt
+++ data/src/main/java/com/mahmoud_darwish/data/local/VolumeEntityDao.kt
@@ -5,18 +5,19 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.mahmoud_darwish.data.local.model.VolumeEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface VolumeEntityDao {
     @Query("SELECT * FROM VolumeEntity")
-    suspend fun getAllChached(): List<VolumeEntity>
+    fun getAllChached(): Flow<List<VolumeEntity>>
 
     @Query("DELETE FROM VolumeEntity")
     suspend fun clearCache()
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(volumes: List<VolumeEntity>)
+    suspend fun insert(volumes: List<VolumeEntity>)
 
     @Query("SELECT * FROM VolumeEntity WHERE LOWER(title) LIKE '%' || LOWER(:title) || '%' OR LOWER(title) = lOWER(:title)")
-    suspend fun volumeSearch(title: String)
+    fun volumeSearch(title: String): Flow<List<VolumeEntity>>
 }
