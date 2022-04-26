5bb63624cd7c9f9a5d251cb23abbf2c42e34ab69
Mahmoud Darwish <imahmwd14@gmail.com>
2022-04-27 03:00:12 +0700

    updated the resource sealed class to contain information about the data source

--- domain/src/main/java/com/mahmoud_darwish/domain/util/Resource.kt
+++ domain/src/main/java/com/mahmoud_darwish/domain/util/Resource.kt
@@ -2,6 +2,8 @@ package com.mahmoud_darwish.domain.util
 
 sealed class Resource<out T> {
     class Success<T>(val data: T) : Resource<T>()
-    object Loading : Resource<Nothing>()
+    class Loading(source: Source = Source.REMOTE) : Resource<Nothing>()
     class Error(val message: String) : Resource<Nothing>()
 }
+
+enum class Source { REMOTE, CACHE }
