683ba92e96d49a4afcf4fa1a848190c0523f06c8
Mahmoud Darwish <imahmwd14@gmail.com>
2022-04-26 21:18:23 +0700

    -

--- domain/src/main/java/com/mahmoud_darwish/domain/util/Resource.kt
+++ domain/src/main/java/com/mahmoud_darwish/domain/util/Resource.kt
@@ -1,6 +1,6 @@
 package com.mahmoud_darwish.domain.util
 
-sealed class Resource<T> {
+sealed class Resource<out T> {
     class Success<T>(val data: T) : Resource<T>()
     object Loading : Resource<Nothing>()
     class Error(val message: String) : Resource<Nothing>()
