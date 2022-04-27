86338c5a3d3921ad50439d5fff4bbe20498c93f1
Mahmoud Darwish <imahmwd14@gmail.com>
2022-04-28 06:44:15 +0700

    corrected mistakes

--- domain/src/main/java/com/mahmoud_darwish/domain/util/Resource.kt
+++ domain/src/main/java/com/mahmoud_darwish/domain/util/Resource.kt
@@ -1,7 +1,7 @@
 package com.mahmoud_darwish.domain.util
 
 sealed class Resource<out T> {
-    class Success<T>(val data: T, source: Source, message: String? = null) : Resource<T>()
+    class Success<T>(val data: T, val source: Source, val message: String? = null) : Resource<T>()
     object Loading : Resource<Nothing>()
     class Error(val message: String) : Resource<Nothing>()
 }
